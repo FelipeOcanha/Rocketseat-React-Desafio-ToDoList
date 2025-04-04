62fb6162fc21505e65d9c703e9c0e21098cac420
Felipe Ocanha <felipe_ocanha@hotmail.com>
2025-04-03 23:31:01 -0300

    Adicionando task

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -3,50 +3,106 @@ import {Header} from './components/Header';
 import {Input} from './components/Input'
 import {Button} from './components/Button'
 import {TaskInfo} from './components/Task/TaskInfo';
-import { useState } from 'react';
+import {ListTask} from './components/Task/ListTask';
+
+import { ChangeEvent, useState } from 'react';
 
 import { PlusCircle } from '@phosphor-icons/react';
 import { EmptyTaskList } from './components/Task/EmptyTaskList';
 
+
+export interface ITask {
+  id: number
+  text: string
+  isChecked: boolean
+}
+
+
 function App() {
-  interface Task {
-    id: number,
-    name: string
-  }
-
-  const taskList: Task[] = [];
-
-
-
+  
+  const [tasks, setTasks] = useState<ITask[]>([])
   const [inputValue, setInputValue] = useState('')
 
+  const checkedTasksCounter = tasks.reduce((prevValue, currentTask) => {
+    if (currentTask.isChecked) {
+      return prevValue + 1
+    }
+
+    return prevValue
+  }, 0)
+
   function handleAddTask() {
+
     if (!inputValue) {
       return
     }
+
+    const newTask: ITask = {
+      id: new Date().getTime(),
+      text: inputValue,
+      isChecked: false,
+    }
+
+    setTasks((state) => [...state, newTask])
+    setInputValue('')
   }
+
+  function handleRemoveTask(id: number) {
+    const filteredTasks = tasks.filter((task) => task.id !== id)
+
+    if (!confirm('Deseja mesmo apagar essa tarefa?')) {
+      return
+    }
+
+    setTasks(filteredTasks)
+  }
+
+  function handleToggleTask({ id, value }: { id: number; value: boolean }) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isChecked: value }
+      }
+
+      return { ...task }
+    })
+
+    setTasks(updatedTasks)
+  }
+
   return (
     <main>
       <Header />
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
-          <Input />
+          <Input onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}/>
 
           <Button onClick={handleAddTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
           </div>
+          
           <div className={styles.tasksList}>
-            <TaskInfo tasksCounter={0} checkedTasksCounter={0} />
-          </div>
-          <div>
-            { taskList.length == 0 ? 
-            ( 
-              < EmptyTaskList />
-            ) : (<div></div>)
-            }
-          </div>
+            <TaskInfo tasksCounter={tasks.length} checkedTasksCounter={checkedTasksCounter} />
+            </div>
+            <div>
+            {tasks.length > 0 ? (
+            <div>
+              {tasks.map((task) => (
+                <ListTask
+                  key={task.id}
+                  data={task}
+                  removeTask={handleRemoveTask}
+                  toggleTaskStatus={handleToggleTask}
+                />
+              ))}
+            </div>
+          ) : (
+            <EmptyTaskList />
+          )}
+            </div>
+         
       </section>
     </main>
   );
--- ToDoList/src/components/Task/EmptyTaskList.tsx
+++ ToDoList/src/components/Task/EmptyTaskList.tsx
@@ -1,12 +1,14 @@
-import styles from './EmptyTaskList.module.css'
-import EmptyLogo from '../../images/Clipboard.png'
+import styles from './EmptyTaskList.module.css';
+import EmptyLogo from '../../images/Clipboard.png';
 
 export function EmptyTaskList() {
-    return (
-        <div className={styles.container}>
-            <img src={EmptyLogo} />
-            <strong>Você ainda não tem tarefas cadastradas</strong>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <img src={EmptyLogo} />
+      <p>
+        <strong>Você ainda não tem tarefas cadastradas</strong>
+        Crie tarefas e organize seus itens a fazer
+      </p>
+    </div>
+  )
 }
